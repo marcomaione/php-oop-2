ab70d63771f7972fffa5d804fd6630c953d8a2e3
marco maione <maionemarco14@gmail.com>
2022-03-16 17:24:10 +0100

    aggiornamento

---
+++ classes/alimentazione.php
@@ -0,0 +1,16 @@
+<?php
+
+
+class alimentazione {
+    
+    public $alimentazione1;
+    public $alimentazione2;
+    public $alimentazione3;
+
+    public function __construct($alimentazione1, $alimentazione2,$alimentazione3) {
+        $this->alimentazione1= $alimentazione1;
+        $this->alimentazione2 = $alimentazione2;
+        $this->alimentazione3 = $alimentazione3;
+    }
+
+}  
--- index.php
+++ index.php
@@ -4,13 +4,17 @@ require_once __DIR__ . '/classes/registered_user.php';
 require_once __DIR__ . '/classes/user.php';
 require_once __DIR__ . '/classes/giochi.php';
 require_once __DIR__ . '/classes/accessori.php';
+require_once __DIR__ . '/classes/alimentazione.php';
+
 
 $utente = new User('Mario',40);
 $utenteRegistrato = new Registered_user('pippo', 'pippo@.it');
 $game = new Giochi('palla', 'corda', 'peluche');
 $accessory = new Accessori('cuccia','guinzaglio','collare');
+$powerSupply = new Alimentazione('mangime secco','scatoletta cibo umido','crocchette al pollo secche');
 
 var_dump($utente);
 var_dump($utenteRegistrato);
 var_dump($game);
 var_dump($accessory);
+var_dump($powerSupply);
